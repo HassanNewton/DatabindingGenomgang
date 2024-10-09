c9f353064b692119c842e1ea587864ae1a0abfdc
HassanNewton <hassan.hussin@newton.se>
2024-10-09 12:28:16 +0200

    Add project files.

---
+++ DatabindingExample/Formulär.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace DatabindingExample
+{
+    /// <summary>
+    /// Interaction logic for Formulär.xaml
+    /// </summary>
+    public partial class Formulär : Window, INotifyPropertyChanged
+    {
+        public Formulär()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged(nameof(firstName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged(nameof(lastName));
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DatabindingExample/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace DatabindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Boundtext
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            Boundtext = "Text från codeBehind";
+        }
+    }
+}
