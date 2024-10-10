5b23991036096043afa340c4f0d87542659f012a
HassanNewton <hassan.hussin@newton.se>
2024-10-10 13:38:34 +0200

    SLiderExempel

---
+++ DatabindingExample/Exempel4.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace DatabindingExample
+{
+    /// <summary>
+    /// Interaction logic for Exempel4.xaml
+    /// </summary>
+    public partial class Exempel4 : Window, INotifyPropertyChanged
+    {
+        public Exempel4()
+        {
+            InitializeComponent();
+            //Sätt datacontext till this
+            DataContext = this;
+        }
+
+        private double sliderValue;
+        public double SliderValue
+        {
+            get { return sliderValue; }
+            set
+            {
+                sliderValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
